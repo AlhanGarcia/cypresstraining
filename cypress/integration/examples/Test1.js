f19189f3dffb026e1f7c08280c201b53cfc04bc0
AlhanGarcia <agpm13@gmail.com>
2022-07-11 16:26:09 -0500

    progress on writing first cypress tests

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -1,7 +1,30 @@
+/// <reference types="Cypress" />
+
 describe('My First Test', () => {
 
     it('Does not do much!', () => {
-      cy.visit('https://www.google.com.mx/?hl=es-419')
+      cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/')
+      cy.get('.search-keyword').type('ca')
+      cy.wait(2000)
+      // Get invisible products as well
+      cy.get('.product').should('have.length', 5)
+      // In cypress get acts as findElement in Selenium (get visible products only)
+      cy.get('.product:visible').should('have.length', 4)
+      // Parent child chaining
+      cy.get('.products').find('.product').should('have.length', 4)
+      // Look for specific elements based on the index (hardcoded index)
+      cy.get('.products').find('.product').eq(2).contains('ADD TO CART').click()
+
+      // Iterate over array of products with EACH
+      cy.get('.products')
+      .find('.product')
+      .each(($el, index, $list) => {
+        const textVeg = $el.find('h4.product-name').text()
+        if(textVeg.includes('Cashews')){
+          cy.wrap($el).find('button').click()
+        }
+      })
+
     })
 
   })
